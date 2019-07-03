9905e78b10e9fa98f78d6f79cb31622c9d440fba
Mariia <mariiapopovych@gmail.com>
2019-07-03 16:39:13 -0700

    add pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
+import Pagination from './common/pagination';
 import { getMovies} from '../services/fakeMovieService';
+import Like from './common/like';
 class Movies extends Component {
     state = {
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4,
+        currentPage: 1 
       };
 
       handleDelete= movie => {
@@ -10,8 +14,23 @@ class Movies extends Component {
           this.setState({movies});
 
       };
+
+      handleLike=movie=> {
+          const movies = [...this.state.movies];
+          const index = movies.indexOf(movie);
+          movie[index] = {...movies[index]};
+          movies[index].liked = !movies[index].liked;
+          this.setState({movies});
+
+      }
+
+      handlePageChange=page=> {
+          this.setState({currentPage: page});
+
+      }
     render() { 
         const { length: count }= this.state.movies;
+        const {pageSize, currentPage} = this.state;
         if (count ===0) return <p>There are no movies in database!</p>
         return (
             <React.Fragment>
@@ -24,6 +43,7 @@ class Movies extends Component {
                     <th>Stock</th>
                     <th>Rate</th>
                     <th></th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -33,11 +53,16 @@ class Movies extends Component {
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
+                    <td><Like liked={movie.liked} onClick={()=>this.handleLike(movie)}/></td>
                     <td><button onClick= {()=>this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                 </tr>
                 ))}
             </tbody>
         </table>
+        <Pagination itemsCount = {count}
+         pageSize = {pageSize} 
+         onPageChange = {this.handlePageChange}
+         currentPage = {currentPage}/>
             </React.Fragment>
         
         );
