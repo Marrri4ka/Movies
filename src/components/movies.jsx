d264dbb620e128336c2662f449fb86e1e3ff6592
Mariia <mariiapopovych@gmail.com>
2019-07-04 00:03:14 -0700

    filtering, sorting

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import Pagination from './common/pagination';
 import { getMovies} from '../services/fakeMovieService';
-import Like from './common/like';
+import {getGenres} from '../services/fakeGenreService';
+import {paginate} from '../utils/paginate';
+import ListGroup from './common/listGroup';
+import MoviesTable from './moviesTable';
+import _ from 'lodash';
 class Movies extends Component {
     state = {
-        movies: getMovies(),
+        movies: [],
         pageSize: 4,
-        currentPage: 1 
+        currentPage: 1,
+        genres: [],
+        sortColumn: {path:'title', order: 'asc'}
       };
+
+      componentDidMount() {
+          const genres=[{_id:'',name:"All Gennres"},...getGenres()]
+        this.setState({movies: getMovies(), genres});
+      }
 
       handleDelete= movie => {
           const movies = this.state.movies.filter(m=> m._id !== movie._id);
@@ -27,43 +38,52 @@ class Movies extends Component {
       handlePageChange=page=> {
           this.setState({currentPage: page});
 
-      }
-    render() { 
+      };
+
+      handleGenreSelect = genre => {
+          this.setState({selectedGenre: genre, currentPage:1});
+      };
+      handleSort=path=>{
+          const sortColumn={...this.state.sortColumn};
+          if(sortColumn.path===path)
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc'
+            else {
+                sortColumn.path = path;
+                sortColumn.order = "asc";
+            }
+          this.setState({sortColumn});           
+
+      };
+     render() { 
         const { length: count }= this.state.movies;
-        const {pageSize, currentPage} = this.state;
+        const {pageSize, currentPage,movies: allMovies,selectedGenre,sortColumn} = this.state;
         if (count ===0) return <p>There are no movies in database!</p>
+        const filtered = selectedGenre && selectedGenre._id
+         ? allMovies.filter (m=> m.genre._id===selectedGenre._id) 
+         : allMovies;
+
+        const sorted=  _.orderBy(filtered,[sortColumn.path],[sortColumn.order]);
+        const movies =paginate(sorted,currentPage,pageSize);
         return (
-            <React.Fragment>
-                <p>Showimg {count} movies in database</p>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Genre</th>
-                    <th>Stock</th>
-                    <th>Rate</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.state.movies.map(movie=>(
-                <tr key={movie._id}>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td><Like liked={movie.liked} onClick={()=>this.handleLike(movie)}/></td>
-                    <td><button onClick= {()=>this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
-        <Pagination itemsCount = {count}
+            <div className='row'>
+                <div className="col-3">
+                    <ListGroup 
+                    selectedItem={this.state.selectedGenre}
+                    items={this.state.genres}
+                     onItemSelect= {this.handleGenreSelect}/></div>
+                <div className="col"><p>Showimg {filtered.length} movies in database</p>
+       <MoviesTable
+        movies={movies}
+         onLike={this.handleLike} 
+         onDelete={this.handleDelete}
+         onSort = {this.handleSort}/>
+        <Pagination 
+        itemsCount = {filtered.length}
          pageSize = {pageSize} 
          onPageChange = {this.handlePageChange}
-         currentPage = {currentPage}/>
-            </React.Fragment>
+         currentPage = {currentPage}/></div>
+                
+            </div>
         
         );
     }
